2e11ebdd5dbe2c11d8b5726a04da2388872cedf2
Dzerdevwarnd <dzerdevwarnd3@gmail.com>
2024-05-16 10:20:22 +0300

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
@@ -34,6 +35,16 @@ async function bootstrap() {
   useContainer(app.select(AppModule), {
     fallbackOnErrors: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3004);
 }
 bootstrap();
